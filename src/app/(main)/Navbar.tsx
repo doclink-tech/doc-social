262ea799acdbc269bc17248741a27379546da19f
darshit890 <88980439+darshit890@users.noreply.github.com>
2024-08-21 00:12:37 +0530

    first commit

---
+++ src/app/(main)/Navbar.tsx
@@ -0,0 +1,18 @@
+import SearchField from "@/components/SearchField";
+import UserButton from "@/components/UserButton";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <header className="sticky top-0 z-10 bg-card shadow-sm">
+      <div className="mx-auto flex max-w-7xl flex-wrap items-center justify-center gap-5 px-5 py-3">
+        <Link href="/" className="text-2xl font-bold text-primary">
+          <Image src={'/DOCLINK.svg'} alt="" width={150} height={150} />
+        </Link>
+        <SearchField />
+        <UserButton className="sm:ms-auto" />
+      </div>
+    </header>
+  );
+}
